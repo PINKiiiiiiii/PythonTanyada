474c06ab06a1ffeba4a901ee0ff7d5585dcc169a
PINKiiiiiiii <105914114+PINKiiiiiiii@users.noreply.github.com>
2022-08-19 16:21:29 +0700

    19/8/65

---
+++ CS01-9.py
@@ -0,0 +1,8 @@
+a = int(input("Enter The First Number : "))
+b = int(input("Enter The Second Number : "))
+c = int(input("Enter The Third Number : "))
+
+if (a>b and a<c):
+    print("condition is True")
+else:
+    print("condition is False")
